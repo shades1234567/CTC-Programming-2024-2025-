694c06c5c4c254cd5cbd6310d51ac182af6f3902
shades1234567 <shayneandrews01@gmail.com>
2025-11-28 13:03:05 -0500

    Add files via upload

---
+++ CH6/CH6EX10/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace CH6EX10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.lblCheck = new System.Windows.Forms.Label();
+            this.lblGame = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Location = new System.Drawing.Point(132, 56);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(100, 20);
+            this.txtGuess.TabIndex = 0;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(28, 59);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(74, 13);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "Enter a Guess";
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(31, 157);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(75, 43);
+            this.btnGuess.TabIndex = 2;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // lblCheck
+            // 
+            this.lblCheck.AutoSize = true;
+            this.lblCheck.Location = new System.Drawing.Point(163, 172);
+            this.lblCheck.Name = "lblCheck";
+            this.lblCheck.Size = new System.Drawing.Size(0, 13);
+            this.lblCheck.TabIndex = 3;
+            // 
+            // lblGame
+            // 
+            this.lblGame.AutoSize = true;
+            this.lblGame.Location = new System.Drawing.Point(285, 63);
+            this.lblGame.Name = "lblGame";
+            this.lblGame.Size = new System.Drawing.Size(0, 13);
+            this.lblGame.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblGame);
+            this.Controls.Add(this.lblCheck);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.txtGuess);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Label lblCheck;
+        private System.Windows.Forms.Label lblGame;
+    }
+}
+
---
+++ CH6/CH6EX10/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Console;
+using System.Globalization;
+
+namespace CH6EX10
+{
+    public partial class Form1 : Form
+    {
+        Random ranNumberGenerator = new Random();
+        bool correct = false;
+        bool correctletter = false;
+        char guess;
+        string[] names = { "Duck", "Tiger", "Cat", "Sheep", "Axolotl" };
+        string guessword;
+        char[] displayword;
+        int randomNumber; public Form1()
+        
+        {
+            InitializeComponent();
+            GenerateWord();
+
+        }      
+
+        private void GenerateWord()
+        {
+            randomNumber = ranNumberGenerator.Next(0, 5);
+            guessword = names[randomNumber];
+            displayword = new char[guessword.Length];
+        }
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+           
+            lblGame.Text = string.Empty;
+            for (int i = 0; i < guessword.Length; ++i)
+            {
+                displayword[i] = '*';
+            }
+            if (!correct)
+            {
+                correctletter = false;
+                lblGame.Text += ("Word: ");
+                for (int i = 0; i < guessword.Length; ++i)
+                {
+                    lblGame.Text += Convert.ToString(displayword[i]);
+                }
+                guess = Convert.ToChar(txtGuess.Text);
+
+
+                for (int i = 0; i < guessword.Length; ++i)
+                {
+                    if (guess == guessword[i])
+                    {
+                        displayword[i] = guess;
+                        correctletter = true;
+                    }
+                }
+
+
+                if (correctletter == true)
+                {
+                    lblCheck.Text = ("Yes! " + guess + " is in the word");
+                }
+                else
+                {
+                    if (correctletter == false)
+                    {
+                        lblCheck.Text = ("Sorry. " + guess + " is not in the word");
+                    }
+                }
+
+
+                correct = true;
+                for (int i = 0; i < guessword.Length; ++i)
+                {
+                    if (displayword[i] != guessword[i])
+                    {
+                        correct = false;
+                    }
+                }
+                if (correct == true)
+                {
+                    lblCheck.Text = ("You Won!");
+                }
+
+            }
+        }
+    }
+}
